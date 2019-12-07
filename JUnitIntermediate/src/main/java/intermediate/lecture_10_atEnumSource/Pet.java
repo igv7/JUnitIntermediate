c136ddf136d946551151346f0ee03c2ba092c5f5
igv7 <46227257+igv7@users.noreply.github.com>
2019-12-08 01:01:48 +0200

    Adding new lessons

---
+++ JUnitIntermediate/src/main/java/intermediate/lecture_10_atEnumSource/Pet.java
@@ -0,0 +1,9 @@
+package intermediate.lecture_10_atEnumSource;
+
+public enum Pet {
+	
+	CAT,
+	DOG,
+	TIGER;
+
+}
---
+++ JUnitIntermediate/src/main/java/intermediate/lecture_11_BlackBoxWhiteBox/WhiteBox.java
@@ -0,0 +1,10 @@
+package intermediate.lecture_11_BlackBoxWhiteBox;
+
+public class WhiteBox {
+	
+	public static int func(int arg) {
+		int result = arg % 2 == 0 ? arg/2 : arg;
+		return result;
+	}
+
+}
